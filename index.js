db90e1f6af3e19162e0c01a41e48293304da0662
Alex Eagle <alexeagle@google.com>
2015-03-17 09:41:32 -0700

    Code review comments

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var through = require('through2');
 var ts2dart = require('ts2dart');
 var which = require('which');
 
-function transpile() {
-    return through.obj(function (file, enc, done) {
+exports = {
+    transpile: function() {
+      return through.obj(function (file, enc, done) {
         file.contents = new Buffer(ts2dart.translateFiles([file.path]));
         file.path = file.path.replace(/.ts$/, '.dart');
         done(null, file);
-    });
-}
-exports.transpile = transpile;
-function format() {
-    return spawn({ cmd: which.sync('dartfmt') });
-}
-exports.format = format;
+    })},
+
+    format: function() {
+        return spawn({cmd: which.sync('dartfmt')});
+    }
+};
