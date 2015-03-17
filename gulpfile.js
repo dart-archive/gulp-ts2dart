8f76b439667fe9daf17bb5a672c9049c17ffad0e
Alex Eagle <alexeagle@google.com>
2015-03-17 09:45:15 -0700

    Hook up the formatter.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
+var formatter = require('gulp-clang-format');
 var gulp = require('gulp');
-var ts = require('gulp-typescript');
 var ts2dart = require('./');
 
+gulp.task('test.check-format', function() {
+  return gulp.src(['*.js', '*.ts']).pipe(formatter.checkFormat());
+});
+
 gulp.task('test', function() {
-    gulp.src('test.ts')
-        .pipe(ts2dart.transpile())
-        .pipe(ts2dart.format())
-        .pipe(gulp.dest('result'));
+  gulp.src('test.ts')
+      .pipe(ts2dart.transpile())
+      .pipe(ts2dart.format())
+      .pipe(gulp.dest('result'));
 });
--- index.js
+++ index.js
@@ -4,14 +4,13 @@ var ts2dart = require('ts2dart');
 var which = require('which');
 
 exports = {
-    transpile: function() {
-      return through.obj(function (file, enc, done) {
-        file.contents = new Buffer(ts2dart.translateFiles([file.path]));
-        file.path = file.path.replace(/.ts$/, '.dart');
-        done(null, file);
-    })},
+  transpile: function() {
+    return through.obj(function(file, enc, done) {
+      file.contents = new Buffer(ts2dart.translateFiles([file.path]));
+      file.path = file.path.replace(/.ts$/, '.dart');
+      done(null, file);
+    })
+  },
 
-    format: function() {
-        return spawn({cmd: which.sync('dartfmt')});
-    }
+  format: function() { return spawn({cmd: which.sync('dartfmt')}); }
 };
