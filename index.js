881bb12916a8336d93e2e540b102632c055059a2
Martin Probst <martin@probst.io>
2015-03-20 12:04:45 -0700

    Print out errors during compilation, but continue.
    Fail compilation once all files have been touched.

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
+var chalk = require('chalk');
+var gutil = require('gulp-util');
 var spawn = require('gulp-spawn');
 var through = require('through2');
 var ts2dart = require('ts2dart');
 var which = require('which');
 
 exports.transpile = function() {
-  return through.obj(function(file, enc, done) {
-    file.contents = new Buffer(ts2dart.translateFile(file.path));
-    file.path = file.path.replace(/.ts$/, '.dart');
-    done(null, file);
-  });
+  var hadErrors = false;
+  return through.obj(
+      function(file, enc, done) {
+        try {
+          var src = ts2dart.translateFile(file.path);
+          file.contents = new Buffer(src);
+          file.path = file.path.replace(/.ts$/, '.dart');
+          done(null, file);
+        } catch (e) {
+          hadErrors = true;
+          if (e.name === 'TS2DartError') {
+            // Sort of expected, log only the message.
+            gutil.log(chalk.red(e.message));
+          } else {
+            // Log the entire stack trace.
+            gutil.log(chalk.red(e.stack));
+          }
+          done(null, null);
+        }
+      },
+      function finished() {
+        if (hadErrors) {
+          gutil.log(chalk.red('ts2dart transpilation failed.'));
+          throw new Error('ts2dart transpilation failed.');
+        } else {
+          gutil.log(chalk.green('ts2dart transpilation succeeded.'));
+        }
+      });
 };
 
 exports.format = function() {
